8f2a7fb8308e73412d8d4baa4dbf719fe53f764c
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-12-17 09:29:10 +0530

    Create 1.c

---
+++ practicle2/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+
+printf("enter the two numbers ");
+scanf("%d%d",&a,&b);
+if(a==b)
+{
+printf(" it is equal");
+}
+else
+{ printf("Not equal");
+}
+ getch();    }
