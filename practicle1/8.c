08cc117cf315fd47d47c02a935bb1b41813a12eb
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-12-17 09:23:30 +0530

    Create 8.c

---
+++ practicle1/8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float a,b,c,s,r,area;
+clrscr();
+printf("enter the three sides ");
+scanf("%f%f%f",&a,&b,&c);
+s=(a+b+c)/2;
+r=s*(s-a)*(s-b)*(s-c);
+area=sqrt(r);
+printf(" area = %f",area);
+getch();
+}
